1d8fe88d7171bb7bb7a691e34bd68c764e904a40
Andre Santos <andrepostech@gmail.com>
2025-02-06 01:13:16 -0300

    Add properties NATS

---
+++ src/main/kotlin/soat7/group61/myvideonotify/messaging/config/NatsConfiguration.kt
@@ -0,0 +1,31 @@
+package soat7.group61.myvideonotify.messaging.config
+
+import io.nats.client.Nats
+import io.nats.client.Connection
+import mu.KotlinLogging
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile("!test")
+class NatsConfiguration(
+    private val natsProperties: NatsProperties
+) {
+
+    companion object {
+        private val log = KotlinLogging.logger { }
+    }
+
+    @Bean
+    fun natsClient(): Connection {
+        try {
+            val connection = Nats.connect(natsProperties.natsServerUrl)
+            log.info { "Connected to NATS server at ${natsProperties.natsServerUrl}" }
+            return connection
+        } catch (ex: Exception) {
+            log.error { "Failed to connect to NATS server: ${ex.message}" }
+            throw ex
+        }
+    }
+}
