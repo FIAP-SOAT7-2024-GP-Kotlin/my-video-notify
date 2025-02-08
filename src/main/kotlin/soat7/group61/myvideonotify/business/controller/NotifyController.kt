35a29cffc490ee5f2918019d1576c4c77a579b2e
Andre Santos <andrepostech@gmail.com>
2025-02-07 21:49:07 -0300

    Add MailerSend Controller

--- src/main/kotlin/soat7/group61/myvideonotify/business/controller/NotifyController.kt
+++ src/main/kotlin/soat7/group61/myvideonotify/business/controller/NotifyController.kt
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import soat7.group61.myvideonotify.business.service.NotifyService
+import soat7.group61.myvideonotify.config.MailerSendProperties
 
 @RestController
 @RequestMapping("/notify")
 class NotifyController(
-    private val notifyService: NotifyService
+    private val notifyService: NotifyService,
 ) {
 
     @PostMapping("/hello")
     suspend fun sayHello(): ResponseEntity<String> {
         val email = Email()
 
-        email.setFrom("My Video", "fiapmyburguer@gmail.com")
-        email.addRecipient("User", "alssantos.482@gmail.com")
+        email.addRecipient("André", "alssantos.482@gmail.com")
         email.setSubject("Fail to process [TESTE]")
         email.setPlain("The process of TESTE must be failed!")
 
